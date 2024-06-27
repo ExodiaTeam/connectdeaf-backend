caeead6108d03f70f38990d4d9e66855b4f84b25
Luís Estevam Rosa Chaves <estevamchaves@alu.ufc.br>
2024-06-26 21:08:27 -0300

    feat: adicionando arquivos nos repositório

---
+++ src/main/java/com/connectdeaf/ConnectdeafProjectApplication.java
@@ -0,0 +1,13 @@
+package com.connectdeaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectdeafProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectdeafProjectApplication.class, args);
+	}
+
+}
