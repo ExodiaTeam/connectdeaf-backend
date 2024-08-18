035753d5c0cc9700d649a2e379922c7bb2e5dd11
Estevam1to <luisestevam33@gmail.com>
2024-08-17 23:48:14 -0300

    feat: configurando projeto

---
+++ src/main/java/com/connectdeaf/ConnectdeafApplication.java
@@ -0,0 +1,12 @@
+package com.connectdeaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectdeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectdeafApplication.class, args);
+	}
+}
