eda53ee1d5a3bb4b1609c4085aa5e5d2b0d2ddab
Estevam1to <luisestevam33@gmail.com>
2024-08-20 23:21:32 -0300

    feat: modificando domain user

--- src/main/java/com/connectdeaf/domain/user/User.java
+++ src/main/java/com/connectdeaf/domain/user/User.java
@@ -1,5 +1,6 @@
 package com.connectdeaf.domain.user;
 
+import com.connectdeaf.domain.address.Address;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -24,4 +26,7 @@ public class User {
     private String email;
     private String password;
     private String phoneNumber;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    private List<Address> addresses;
 }
