f1fd282c681a465ac9406ba34b50d1b0469a8040
Estevam1to <luisestevam33@gmail.com>
2024-08-31 09:03:38 -0300

    feat: criando entity role

---
+++ src/main/java/com/connectdeaf/domain/user/Role.java
@@ -0,0 +1,29 @@
+package com.connectdeaf.domain.user;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Table(name = "TB_ROLES")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Getter
+    public enum Values {
+        ROLE_USER(1L),
+        ROLE_PROFESSIONAL(2L),
+        ROLE_ADMIN(3L);
+
+        final long id;
+
+        Values(long id) {
+            this.id = id;
+        }
+    }
+}
